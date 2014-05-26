b511c08b9ca58b14485d42188e86cf943fcc972c
Danilo <danilozacyac@gmail.com>
2014-05-26 13:58:07 -0500

    First Commit

---
+++ PdfQuinielas/Singleton/EquiposSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using PdfQuinielas.Models;
+using Quiniela.Dao;
+
+namespace PdfQuinielas.Singleton
+{
+    public class EquiposSingleton
+    {
+        private static ObservableCollection<Equipos> equipos;
+
+        private EquiposSingleton() { }
+
+        public static ObservableCollection<Equipos> EquiposSin
+        {
+            get
+            {
+                if (equipos == null)
+                    equipos = EquiposModel.GetEquipos();
+
+                return equipos;
+            }
+        }
+    }
+}
