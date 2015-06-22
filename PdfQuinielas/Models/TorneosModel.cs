591a3215c87ed1d7c2cee8be06dd720116ecf9e2
Danilo <danilozacyac@gmail.com>
2015-06-22 14:03:38 -0500

    Some changes

--- PdfQuinielas/Models/TorneosModel.cs
+++ PdfQuinielas/Models/TorneosModel.cs
@@ -6,48 +6,57 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Windows;
 using PdfQuinielas.Dao;
-//using ScjnUtilities;
+using ScjnUtilities;
 
 namespace PdfQuinielas.Models
 {
     public class TorneosModel
     {
-
         private readonly string connectionString = ConfigurationManager.ConnectionStrings["QuinielaMundialConnectionString"].ConnectionString;
 
         public ObservableCollection<Torneos> GetTorneos()
         {
             ObservableCollection<Torneos> listaPartidos = new ObservableCollection<Torneos>();
+            SqlConnection connection = new SqlConnection(connectionString);
 
             try
             {
-                //string cstr = @"Data Source=WIN-KT1RP3JF2N6\MISERVER;Initial Catalog=QuinielaMundial;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
-                using (SqlConnection conn = new SqlConnection(connectionString))
+                
+                connection.Open();
+
+                string selstr = "Select * FROM Torneos ORDER BY IdTorneo";
+                SqlCommand cmd = new SqlCommand(selstr, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
                 {
-                    conn.Open();
+                    Torneos partido = new Torneos();
 
-                    string selstr = "Select * FROM Torneos ORDER BY IdTorneo";
-                    SqlCommand cmd = new SqlCommand(selstr, conn);
-                    SqlDataReader reader = cmd.ExecuteReader();
+                    partido.IdTorneo = Convert.ToInt32(reader["IdTorneo"]);
+                    partido.Torneo = reader["Torneo"].ToString();
+                    partido.Year = Convert.ToInt32(reader["Year"]);
 
-                    while (reader.Read())
-                    {
-                        Torneos partido = new Torneos();
-
-                        partido.IdTorneo = Convert.ToInt32(reader["IdTorneo"]);
-                        partido.Torneo = reader["Torneo"].ToString();
-                        partido.Year = Convert.ToInt32(reader["Year"]);
-
-                        listaPartidos.Add(partido);
-                    }
-                    conn.Close();
+                    listaPartidos.Add(partido);
                 }
+                connection.Close();
             }
-            catch (SqlException)
+            catch (SqlException ex)
             {
+                string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+                MessageBox.Show("Error ({0}) : {1}" + ex.Source + ex.Message, methodName, MessageBoxButton.OK, MessageBoxImage.Warning);
+                ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
+            }
+            catch (Exception ex)
+            {
+                string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+                MessageBox.Show("Error ({0}) : {1}" + ex.Source + ex.Message, methodName, MessageBoxButton.OK, MessageBoxImage.Warning);
+                ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
             }
             finally
             {
+                connection.Close();
             }
 
             return listaPartidos;
@@ -64,8 +73,7 @@ namespace PdfQuinielas.Models
 
             try
             {
-                //torneo.IdTorneo = DataBaseUtilities.GetNextIdForUse("Torneos", "IdTorneo", connection);
-
+                torneo.IdTorneo = DataBaseUtilities.GetNextIdForUse("Torneos", "IdTorneo", connection);
 
                 string sqlCadena = "SELECT * FROM Torneos WHERE IdTorneo = 0";
 
@@ -80,7 +88,6 @@ namespace PdfQuinielas.Models
                 dr["Year"] = torneo.FechaInicio.Value.Year;
                 dr["FechaInicio"] = torneo.FechaInicio;
 
-
                 dataSet.Tables["Torneos"].Rows.Add(dr);
 
                 dataAdapter.InsertCommand = connection.CreateCommand();
@@ -94,22 +101,20 @@ namespace PdfQuinielas.Models
                 dataAdapter.Update(dataSet, "Torneos");
                 dataSet.Dispose();
                 dataAdapter.Dispose();
-
-             
             }
             catch (SqlException ex)
             {
                 string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
 
                 MessageBox.Show("Error ({0}) : {1}" + ex.Source + ex.Message, methodName, MessageBoxButton.OK, MessageBoxImage.Warning);
-                //ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
+                ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
             }
             catch (Exception ex)
             {
                 string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
 
                 MessageBox.Show("Error ({0}) : {1}" + ex.Source + ex.Message, methodName, MessageBoxButton.OK, MessageBoxImage.Warning);
-                //ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
+                ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
             }
             finally
             {
@@ -153,22 +158,20 @@ namespace PdfQuinielas.Models
                 dataAdapter.Update(dataSet, "Torneos");
                 dataSet.Dispose();
                 dataAdapter.Dispose();
-
-
             }
             catch (SqlException ex)
             {
                 string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
 
                 MessageBox.Show("Error ({0}) : {1}" + ex.Source + ex.Message, methodName, MessageBoxButton.OK, MessageBoxImage.Warning);
-                //ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
+                ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
             }
             catch (Exception ex)
             {
                 string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
 
                 MessageBox.Show("Error ({0}) : {1}" + ex.Source + ex.Message, methodName, MessageBoxButton.OK, MessageBoxImage.Warning);
-                //ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
+                ErrorUtilities.SetNewErrorMessage(ex, methodName, 0);
             }
             finally
             {
